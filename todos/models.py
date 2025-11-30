a39f895fc6eb0563adf0089b8badc6e6d4ea5bf4
mindungil <alswnsrlf12@naver.com>
2025-11-30 15:26:54 +0900

    feat: DB 구성

--- todos/models.py
+++ todos/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
 
 class Todo(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    completed = models.BooleanField(default=False)
+    start_at = models.DateTimeField(default=timezone.now)
+    end_at = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['is_completed', 'end_at', '-created_at']
 
     def __str__(self):
         return self.title
+    
+    def clean(self):
+        if self.end_at and self.end_at < self.start_at:
+            raise ValidationError("마감일은 시작일보다 이후여야 합니다.")
+    
